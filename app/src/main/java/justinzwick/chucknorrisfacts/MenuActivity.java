139218c15cba7d612b2733d78accbe447b306fa6
Justin Zwick <zwickyj@gmail.com>
2016-06-19 18:41:42 -0400

    initialized the three menu buttons in onCreate()

--- app/src/main/java/justinzwick/chucknorrisfacts/MenuActivity.java
+++ app/src/main/java/justinzwick/chucknorrisfacts/MenuActivity.java
@@ -2,12 +2,28 @@ package justinzwick.chucknorrisfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button aboutButton, settingsButton, startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        aboutButton = (Button) findViewById(R.id.aboutButton);
+        settingsButton = (Button) findViewById(R.id.settingsButton);
+        startButton = (Button) findViewById(R.id.startButton);
+
+
+
+
+
+
+
+
+
     }
 }
