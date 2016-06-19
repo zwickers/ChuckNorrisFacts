76799f992744aa1531d275d23f4d73c851950df0
Justin Zwick <zwickyj@gmail.com>
2016-06-19 18:22:27 -0400

    added MenuActivity class and corresponding xml file

---
+++ app/src/main/java/justinzwick/chucknorrisfacts/MenuActivity.java
@@ -0,0 +1,13 @@
+package justinzwick.chucknorrisfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
