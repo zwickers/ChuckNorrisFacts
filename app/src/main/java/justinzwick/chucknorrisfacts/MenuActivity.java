38b1f93920a0b8a5d0a65bc78ec84ddb080f97a6
Justin Zwick <zwickyj@gmail.com>
2016-06-19 19:09:20 -0400

    added empty click listeners to menu buttons

--- app/src/main/java/justinzwick/chucknorrisfacts/MenuActivity.java
+++ app/src/main/java/justinzwick/chucknorrisfacts/MenuActivity.java
@@ -2,6 +2,7 @@ package justinzwick.chucknorrisfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
@@ -12,18 +13,29 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
         aboutButton = (Button) findViewById(R.id.aboutButton);
         settingsButton = (Button) findViewById(R.id.settingsButton);
         startButton = (Button) findViewById(R.id.startButton);
 
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
-
-
+            }
+        });
     }
 }
