de3aa8cf2ee66c0e3bf1b7e2ba2487ff4915614f
Justin Zwick <zwickyj@gmail.com>
2016-06-20 22:41:00 -0400

    changed AboutDialog message text

--- app/src/main/java/justinzwick/chucknorrisfacts/AboutDialog.java
+++ app/src/main/java/justinzwick/chucknorrisfacts/AboutDialog.java
@@ -12,10 +12,10 @@ import android.os.Bundle;
 public class AboutDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
+        // Used Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("This app gives 100% truthful facts about Chuck Norris. Enjoy responsibly.")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        builder.setMessage("This app gives 100% truthful facts about Chuck Norris. Enjoy!")
+                .setPositiveButton("OK!", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         if (dialog != null) {
                             dialog.cancel();
