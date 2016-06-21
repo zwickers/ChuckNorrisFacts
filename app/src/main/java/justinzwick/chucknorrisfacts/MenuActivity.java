d01b4170b7e716cc3e6a81da9869e47d999ed659
Justin Zwick <zwickyj@gmail.com>
2016-06-20 22:08:53 -0400

    created about fragment class and correspond xml file

--- app/src/main/java/justinzwick/chucknorrisfacts/MenuActivity.java
+++ app/src/main/java/justinzwick/chucknorrisfacts/MenuActivity.java
@@ -1,14 +1,18 @@
 package justinzwick.chucknorrisfacts;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MenuActivity extends AppCompatActivity {
 
     private Button aboutButton, settingsButton, startButton;
+    private ImageView mImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +21,15 @@ public class MenuActivity extends AppCompatActivity {
         aboutButton = (Button) findViewById(R.id.aboutButton);
         settingsButton = (Button) findViewById(R.id.settingsButton);
         startButton = (Button) findViewById(R.id.startButton);
-
         aboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
 
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
 
